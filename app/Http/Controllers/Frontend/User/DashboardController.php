c7a070a1326649bdef026c0d7c669077005995f0
will@prime.ms <will@prime.ms>
2016-03-16 04:00:54 -0400

    add active sample checkouts to user dashboard

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend\User;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Asset;
+use App\Models\Checkout;
 
 /**
  * Class DashboardController
@@ -17,7 +19,19 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('frontend.user.dashboard')
-            ->withUser(access()->user());
+        $checkouts = Checkout::where('user_id', '=', auth()->user()->id)->get();
+        $assetsIn = [];
+        foreach ($checkouts as $checkout){
+            $assetsIn[] = $checkout->asset_id;
+        }
+
+        $assets = Asset::whereIn('id', $assetsIn)->get();
+
+        $assets->load('mfr', 'location', 'activeCheckout.dealer', 'activeCheckout.dealer.dealership');
+
+//        return view('frontend.user.dashboard');
+//            ->withUser(access()->user());
+
+        return view('frontend.assets.samples', compact('assets'));
     }
 }
