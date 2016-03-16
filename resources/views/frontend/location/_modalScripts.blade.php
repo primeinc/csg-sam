f107774ee3210295a9c7534cdb30d27eb175d38c
will@prime.ms <will@prime.ms>
2016-03-16 05:13:40 -0400

    fix location ajax

--- resources/views/frontend/location/_modalScripts.blade.php
+++ resources/views/frontend/location/_modalScripts.blade.php
@@ -1,20 +1,18 @@
 @push('scripts')
 <script>
-    var locations;
     $(function($){
         $('button.location').click(function(ev){
-            if(!locations) {
-                locations = $.getJSON("{!! route('api.get.all', 'locations') !!}");
-            }
             ev.preventDefault();
             var uid = $(this).data('id');
             $.get('/samples/' + uid + '/edit/location', function(html){
-                $('#dynModal .modal-content').html(html);
-                $('#dynModal').modal('show', {backdrop: 'static'});
-                $("#location\\[name\\]")
-                        .select2({
-                            data: locations.responseJSON.items
-                        });
+                $.getJSON("{!! route('api.get.all', 'locations') !!}", function(data) {
+                    $('#dynModal .modal-content').html(html);
+                    $('#dynModal').modal('show', {backdrop: 'static'});
+                    $("#location\\[name\\]")
+                            .select2({
+                                data: data.items
+                            });
+                });
             });
         });
     });
