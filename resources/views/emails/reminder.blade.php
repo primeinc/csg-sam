57f5f7124b40f2d8df34078ad4e53cccc17a82a8
will@prime.ms <will@prime.ms>
2016-03-18 01:26:17 -0400

    typo

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -8,7 +8,7 @@
     <br>
     <p>Thank you for the opportunity to provide a sample for your project.</p>
     <p>Per our records, you have borrowed a(n) {!! $checkout->asset->description !!} by {!! $checkout->asset->mfr->name !!} from us on {!! $checkout->created_at->toFormattedDateString() !!}.</p>
-    <p>The chair was due back on {!! $checkout->expected_return_date->toFormattedDateString() !!}, approximately {!! $checkout->expected_return_date->diffForHumans() !!}.</p>
+    <p>The sample was due back on {!! $checkout->expected_return_date->toFormattedDateString() !!}, approximately {!! $checkout->expected_return_date->diffForHumans() !!}.</p>
     <p>Please make arrangements to have the sample returned to us as soon as possible.</p>
     <p>Let us know if you need anything else.</p>
     <p>Thank you.</p>
